279dfb0637e395d36e534655d310a407553b8091
Juan Pernumian <juan.pernumian@gmail.com>
2017-05-17 14:54:44 -0300

    update json

--- js/script.js
+++ js/script.js
@@ -18,6 +18,7 @@ saveButton.click(function () {
 // LISTA DE CONFIRMADOS!
 var invitedPep = {
 	"invitados": [
+		{ "name":"Sol", "subsecretary":"Economica", "direction":"PAPP"  },
 		{ "name":"Puchu", "subsecretary":"SSCI", "direction":"GEDI"  },
 		{ "name":"Florchi", "subsecretary":"SSCI", "direction":"GEDI"  },
 		{ "name":"Ernesto", "subsecretary":"Ciudad Inteligente", "direction":"GEDI"  },
