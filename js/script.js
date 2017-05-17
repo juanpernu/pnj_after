743d88a1f5416a43d8b915300725aed92e60d636
Juan Pernumian <juan.pernumian@gmail.com>
2017-05-17 13:51:39 -0300

    add listInvited func

--- js/script.js
+++ js/script.js
@@ -11,3 +11,40 @@ saveButton.click(function () {
     $('.question').text('¿Querés saber quienes vienen?');
     inviteList.css('display', 'block');
 })
+
+
+
+
+// LISTA DE CONFIRMADOS!
+var invitedPep = {
+	"invitados": [
+		{ "name":"Ponja", "subsecretary":"Ciudad Inteligente", "direction":"DGGEDI"  },
+		{ "name":"Lucho", "subsecretary":"Ciudad Inteligente", "direction":"DGGEDI"  },
+		{ "name":"Sol", "subsecretary":"Ciudad Inteligente", "direction":"DGGEDI"  },
+		{ "name":"Puchu", "subsecretary":"Ciudad Inteligente", "direction":"DGGEDI"  }
+	]
+}
+
+
+
+
+
+var listInvited = ( function () {
+
+	var name;
+	var subse;
+	var direction;
+	var listElement = $('.invited-list');
+
+	for (var i = 0; i < invitedPep.invitados.length; i++) {
+		name = invitedPep.invitados[i].name;
+		subse = invitedPep.invitados[i].subsecretary;
+		direction = invitedPep.invitados[i].direction;
+
+		listElement.append('<li> <strong>' + name + '</strong> ' + '<span>' + subse + ', ' + direction + '</span> </li>');
+	}
+})()
+
+
+
+
